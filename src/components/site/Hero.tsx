9419928d8394d08566ebab7a8073fc7b433dc76b
leoking1978glovo <andres.ganoderma@gmail.com>
2026-08-31 11:57:48 +0200

    cambiar nuevo tradicion que se saborea

--- src/components/site/Hero.tsx
+++ src/components/site/Hero.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import heroBg from "@/assets/hero-bg.jpg";
 
-const words = ["Saborea", "la", "Revolución"];
+const words = ["Tradicion", "que", "se saborea"];
 
 export function Hero() {
   return (
