4b472cc62df82ab59d6e0dff72b15e116f3c4808
leoking1978glovo <andres.ganoderma@gmail.com>
2026-08-31 12:21:20 +0200

    saboriar 3

--- src/components/site/Hero.tsx
+++ src/components/site/Hero.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import heroBg from "@/assets/hero-bg.jpg";
 
-const words = ["Tradicion", "Para", "Saboriar en Familia"];
+const words = ["Tradicion", "Para","Saboriar"];
 
 export function Hero() {
   return (
